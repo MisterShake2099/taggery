a39b1897406a04f7164cc84ff7ba08d8fe38448a
MisterShake2099 <mistershake2099@gmail.com>
2014-01-08 11:21:13 -0800

    Updated parameter info. Removed "mode" for now.
    Script will default to "copy" and the "replace" option will be
    disregarded for now.

--- taggery.py
+++ taggery.py
@@ -1,8 +1,8 @@
-def taggery(location, mode):
+def taggery(location):
 
 # @param location:
-#		the location of a folder containing audio files to be processed by the script
-# @param mode:
-#		the mode in which the script should run.
-#		modes are:
-#			
+#		The location of a folder containing audio files to be processed by the script
+# @param mode: (to be implemented later)
+#		The mode in which the script should run. Will default to "copy".
+#			replace: new files with updated filenames will be written and the base file will be deleted
+#			copy: new files with updated filenames will be created. Base files will be preserved.
