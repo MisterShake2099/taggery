71badfdf85fa926da8393fa55bf25bac3b2eb2b5
MisterShake2099 <mistershake2099@gmail.com>
2014-01-07 17:20:12 -0800

    Added some parameter information.
    Added some parameter information.

--- taggery.py
+++ taggery.py
@@ -1,1 +1,8 @@
 def taggery(location, mode):
+
+# @param location:
+#		the location of a folder containing audio files to be processed by the script
+# @param mode:
+#		the mode in which the script should run.
+#		modes are:
+#			
