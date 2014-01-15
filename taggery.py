936cecf8fc3055b3e822839ee47477a44b97a4c8
MisterShake2099 <mistershake2099@gmail.com>
2014-01-14 17:11:55 -0800

    Small amount of work.
    Small amount of work.

--- taggery.py
+++ taggery.py
@@ -1,36 +1,38 @@
 #!/usr/bin/python
 
-# This script creates new filenames for music files based on
-# tag information. It currently does this by copying the
-# original file with a new name. The ability to then delete
-# the original file (simulating an overwrite) will be added
-# at a later date. The purpose of this renaming is to reduce
-# filepath length for music files by naming them in a way
-# that is organized as if folder trees were used AND to avoid
-# a situation in which tags cannot be read thus rendering
-# much of the metadata for a particular music file unusable.
+# This script creates new filenames for music files based on tag information.
+# It currently does this by copying the original file with a new name. The
+# ability to then delete the original file (simulating an overwrite) will be
+# added at a later date. The purpose of this renaming is to reduce filepath
+# length for music files by naming them in a way that is organized as if
+# folder trees were used AND to avoid a situation in which tags cannot be
+# read thus rendering much of the metadata for a particular music file
+# unusable.
 
 # @title:		taggery
 
 # @author:		MisterShake2099
 
-# @location:	The location of a folder containing audio
-#				files to be processed by the script.
+# @location:	The location of a folder containing audio files to be
+#				processed by the script.
 
 # @mode: (to be implemented later)
-#				The mode in which the script should run.
-#				Will default to "copy".
-#				replace:	New files with updated filenames
-#							will be written and the base file
-#							will be deleted.
-#				copy:		New files with updated filenames
-#							will be created. Base files will
-#							be preserved.
+#				The mode in which the script should run. Default to "copy".
+#				replace:	New files with updated filenames will be written
+#							and the base file will be deleted.
+#				copy:		New files with updated filenames will be created.
+#							Base files will be preserved.
 
 def taggery(location):
 
-	# grab and store all appropriate fields from each audio
-	# file in 'location'
+	# store all appropriate fields from each audio file in 'location'
+
+	fields = []
+
+	for each file in location:
+		
 
 
-	# create copy of each and use stored fields to name file
+	# create copy of each file and use stored fields to name file
+
+	for each in fields
