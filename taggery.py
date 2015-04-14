92453ee6991a0d2ba8a2f1f8e1df6aa41d8c683a
MisterShake2099 <mistershake2099@gmail.com>
2015-04-14 15:27:49 -0700

    mostly args invocation stuff

--- taggery.py
+++ taggery.py
@@ -5,20 +5,15 @@
 # ability to then delete the original file (simulating an overwrite) will be
 # added at a later date.
 
-# @title:		taggery
-# @author:		MisterShake2099
-# @location:	The location of a folder containing audio file(s) to be
-#				processed by the script.
+# @title: taggery
+# @author: MisterShake2099
 
-# @mode: (to be implemented later)
-#				The mode in which the script should run. Default to "copy".
-#				replace:	New files with updated filenames will be written
-#							and the base file will be deleted.
-#				copy:		New files with updated filenames will be created.
-#							Base files will be preserved.
+# Command line arguments:
+# @src: a relative or absolute filepath containing source audio files.
+# @dst: a relative or absolute filepath to put processed audio files.
+# @mode: an optional argument specifying which mode to use.
 
-import sys
-import shutil
+import argparse
 
 import mutagen.FLAC
 import mutagen.ID3v2
@@ -27,32 +22,34 @@ import mutagen.APEv2
 import mutagen.MP4
 import mutagen.ASF
 
-# accept two args: first a directory, second a mode
-args = os.system(sys.argv[1], sys.argv[2])
+parser = argparse.ArgumentParser()
+parser.add_argument("src", help="filepath to source directory")
+parser.add_argument("dst", help="filepath to destination directory")
+parser.add_argument("mode", help="list how to invoke mode options here")
+args = parser.parse_args()
+
+print(args.src)
+print(args.dst)
+print(args.mode)
+
+fields = args.src
 
 def taggery(dir):
 
-	# Store all appropriate fields from each audio file in 'location'.
-	# Metadata order: track number, title, length, (album artist/artist),
-	# album, notes.
-	# Album Artist will be used for file naming when available, otherwise
-	# Artist will be used.
+    # Store all appropriate fields from each audio file in 'location'.
+    # Metadata order: track number, title, length, (album artist/artist),
+    # album, notes.
+    # Album Artist will be used for file naming when available, otherwise
+    # Artist will be used.
 
-	fields = []
-
-	i = 0;
-
-	for file in dir:
+    for file in fields:
         if file is FLAC:
-        	audio = FLAC("example.flac")
+            audio = FLAC("example.flac")
         if file is MP3:
-        	audio = MP3("example.mp3")
+            audio = MP3("example.mp3")
         if file is MP4:
-        	audio = MP4("example.mp4")
+            audio = MP4("example.mp4")
 
+    # create copy of each file and use stored fields to name file
 
-	# create copy of each file and use stored fields to name file
-
-	for each in fields
-
-	copyfile(src, dst)
+    copyfile(args.src, args.dst)
