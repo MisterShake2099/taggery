75971aff69573025c56bc77a5cfda694791ef796
MisterShake2099 <mistershake2099@gmail.com>
2015-04-15 10:59:00 -0700

    more or less done with command line parsing

--- taggery.py
+++ taggery.py
@@ -12,6 +12,7 @@
 # @src: a relative or absolute filepath containing source audio files.
 # @dst: a relative or absolute filepath to put processed audio files.
 # @mode: an optional argument specifying which mode to use.
+# Modes are 'copy' and 'overwrite'.
 
 import argparse
 
@@ -22,11 +23,26 @@ import mutagen.APEv2
 import mutagen.MP4
 import mutagen.ASF
 
+# Receive and parse invocation.
 parser = argparse.ArgumentParser()
-parser.add_argument("src", help="filepath to source directory")
-parser.add_argument("dst", help="filepath to destination directory")
-parser.add_argument("mode", help="list how to invoke mode options here")
+parser.add_argument(
+    "src",
+    help="filepath to source directory")
+parser.add_argument(
+    "dst",
+    help="filepath to destination directory (used only in copy mode)")
+parser.add_argument(
+    "--mode",
+    choices=["copy", "overwrite"],
+    default="copy",
+    help="Select a mode. Defaults to copy.")
 args = parser.parse_args()
+
+print("Running '{}'".format(__file__))
+
+# Set destination to be same as source when destination unspecified.
+if args.dst is None:
+    args.dst = args.src
 
 print(args.src)
 print(args.dst)
@@ -34,7 +50,8 @@ print(args.mode)
 
 fields = args.src
 
-def taggery(dir):
+
+def taggery():
 
     # Store all appropriate fields from each audio file in 'location'.
     # Metadata order: track number, title, length, (album artist/artist),
