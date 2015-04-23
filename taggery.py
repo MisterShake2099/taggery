3d4364c2f511b093fb883b4ea8aacd0b424f9cef
MisterShake2099 <mistershake2099@gmail.com>
2015-04-23 10:25:01 -0700

    refactored to get rid of PEP-related linter warnings

--- taggery.py
+++ taggery.py
@@ -1,54 +1,58 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-# This script creates new filenames for music files based on tag information.
-# It currently does this by copying the original file with a new name. The
-# ability to then delete the original file (simulating an overwrite) will be
-# added at a later date.
+"""
+This script creates new filenames for music files based on tag information.
+It currently does this by copying the original file with a new name. The
+ability to then delete the original file (simulating an overwrite) will be
+added at a later date.
 
 # @title: taggery
 # @author: MisterShake2099
 
 # Command line arguments:
 # @src: a relative or absolute filepath containing source audio files.
-# @dst: a relative or absolute filepath to put processed audio files.
-# @mode: an optional argument specifying which mode to use.
-# Modes are 'copy' and 'overwrite'.
+@dst: a relative or absolute filepath to put processed audio files.
+@mode: an optional argument specifying which mode to use.
+Modes are 'copy' and 'overwrite'.
+"""
 
 import argparse
 
 from mutagen.flac import FLAC
 from mutagen.mp3 import MP3
 from mutagen.id3 import ID3, TIT2
-from mutagen.ogg import OGG
+from mutagen.ogg import OggPage #, OggFileType
 from mutagen.mp4 import MP4
 
-# Receive and parse invocation.
-parser = argparse.ArgumentParser(description="parsing the input")
-parser.add_argument(
-    "src",
-    help="filepath to source directory")
-parser.add_argument(
-    "dst",
-    default="./destination",
-    help="filepath to destination directory (used only in copy mode)")
-parser.add_argument(
-    "--mode",
-    choices=["copy", "overwrite"],
-    default="copy",
-    help="Select a mode. Defaults to copy.")
-args = parser.parse_args()
-
-print("Running '{}'".format(__file__))
-
-print(args.src)
-print(args.dst)
-print(args.mode)
-
-
-fields = args.src
-
-
 def taggery():
+    """ the main function """
+
+
+    # Receive and parse invocation.
+    parser = argparse.ArgumentParser(description="parsing the input")
+    parser.add_argument(
+        "src",
+        help="filepath to source directory")
+    parser.add_argument(
+        "dst",
+        default="./destination",
+        help="filepath to destination directory (used only in copy mode)")
+    parser.add_argument(
+        "--mode",
+        choices=["copy", "overwrite"],
+        default="copy",
+        help="Select a mode. Defaults to copy.")
+    args = parser.parse_args()
+
+    print("Running '{}'".format(__file__))
+
+    print(args.src)
+    print(args.dst)
+    print(args.mode)
+
+
+    fields = args.src
+
 
     for file in fields:
         if file is FLAC:
@@ -61,8 +65,8 @@ def taggery():
             audio = MP3("example.mp3")
         if file is MP4:
             audio = MP4("example.mp4")
-        if file is OGG:
-            audio = OGG("example.ogg")
+        if file is OggPage:
+            audio = OggPage("example.ogg")
 
     print(audio)
 
